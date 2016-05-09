e0453d4818e48a6274d864a570edf2866f747e5e
Em Ax <emilia.axen@gmail.com>
2016-05-08 21:17:33 -0700

    fixed fragment

--- TrackYouFy/app/src/main/java/com/example/em/trackyoufy/YouTubeFragment.java
+++ TrackYouFy/app/src/main/java/com/example/em/trackyoufy/YouTubeFragment.java
@@ -1,9 +1,30 @@
 package com.example.em.trackyoufy;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by emiliaaxen on 16-05-05.
  */
 public class YouTubeFragment extends Fragment {
+
+    public YouTubeFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_youtube, container,
+                false);
+        return view;
+
+    }
 }
+
+
+
