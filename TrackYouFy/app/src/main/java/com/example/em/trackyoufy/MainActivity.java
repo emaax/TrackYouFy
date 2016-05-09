94ef81dedda00213dfce9f0063c477c21a3c125f
Em Ax <emilia.axen@gmail.com>
2016-05-09 13:35:39 -0700

    master branch updated

--- TrackYouFy/app/src/main/java/com/example/em/trackyoufy/MainActivity.java
+++ TrackYouFy/app/src/main/java/com/example/em/trackyoufy/MainActivity.java
@@ -2,18 +2,14 @@ package com.example.em.trackyoufy;
 
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -147,59 +143,3 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 }
 
-   /* DrawerLayout mDrawerLayout;
-    NavigationView mNavigationView;
-    FragmentManager mFragmentManager;
-    FragmentTransaction mFragmentTransaction;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        mNavigationView = (NavigationView) findViewById(R.id.navigation_drawer);
-
-
-
-        mFragmentManager = getSupportFragmentManager();
-        mFragmentTransaction = mFragmentManager.beginTransaction();
-        mFragmentTransaction.replace(R.id.containerView, new Fragment()).commit();
-
-
-        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                mDrawerLayout.closeDrawers();
-                Fragment fragment = null;
-
-
-                if (menuItem.getItemId() == R.id.nav_item_spotify) {
-                    fragment = new SpotifyFragment();
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView, new SpotifyFragment()).commit();
-
-                }
-
-                if (menuItem.getItemId() == R.id.nav_item_youtube) {
-                    fragment = new YouTubeFragment();
-                    FragmentTransaction fragTrans = mFragmentManager.beginTransaction();
-                    fragTrans.replace(R.id.containerView, new YouTubeFragment()).commit();
-                }
-
-                return false;
-            }
-
-        });
-
-
-        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
-        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name,
-                R.string.app_name);
-
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-        mDrawerToggle.syncState();
-
-    }
-}
-*/
